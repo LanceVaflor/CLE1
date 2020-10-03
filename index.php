a7d252fbe8e87b644b5d45c9be58dc05c2d49264
Lance <60122560+LanceVaflor@users.noreply.github.com>
2020-10-03 19:27:06 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,185 @@
+<html>
+    <head>
+        <title>The Galactic Empire</title>
+        <link rel= "stylesheet" type= "text/css" href= "css/styles.css"/>
+    </head>
+        <body>
+            <h1 class="title">The Galactic Empire</h1>
+            <div class="body"> 
+             <header>
+                <div class="tcontainer">
+
+
+                    <div id=helmet-top></div>
+                    <div id=helmet-mid></div>
+                    <div id=helmet-left-stripe1></div>
+                    <div id=helmet-left-stripe2></div>
+                    <div id=helmet-left-stripe3></div>
+                    <div id=helmet-right-stripe1></div>
+                    <div id=helmet-right-stripe2></div>
+                    <div id=helmet-right-stripe3></div>
+                  
+                  
+                    <div id=helmet-middle></div>
+                    <div id=helmet-left></div>
+                    <div id=helmet-right></div>
+                    <div id=eyes>
+                  
+                      <div id=lefteye></div>
+                      <div id=righteye></div>
+                  
+                    </div>
+                  
+                    <div id=mouth></div>
+                    <div id=mouth-bottom></div>
+                  
+                    <div id=helmet-bottom-left></div>
+                    <div id=helmet-bottom-left-inside></div>
+                    <div id=helmet-bottom-right></div>
+                    <div id=helmet-bottom-right-inside></div>
+                </div>
+
+                <div id="nav">
+                    <a href="lightsaber.html" id="lightsabers">Lightsabers</a>
+                    <a href="blasters.html" id="blasters">Blasters</a>
+                    <a href="archive.html" id="archive">Jedi Archive</a>
+                    <a href="galaxy.html" id="galaxy">Galaxies</a> 
+                </div>
+             </header>
+            </div>
+            <div class = "content">
+               <div class = "textcontent">
+                <h1>Welcome to the Galactic Empire!</br>
+                <br>Founded and ruled by Galactic Emperor Sheev Palpatine, who was secretly in fact the</br>
+                <br>Dark Lord of the Sith Darth Sidious - the new regime rose to power with a groundswell of</br>
+                <br>populist support, promising peace and stability from the destructive Clone Wars.</br></h1>
+               </div>
+               <div class = "sabercontainer">
+                  <div class="saber">
+                    </div>
+             </div> 
+
+                <div class = "droidcontainer">  
+                    <div class="droid">
+                        <div class="droid-head">
+                        <div class="head-top">
+                        <div class="top">
+                          <div class="eye"></div>
+                        </div>
+                      </div>
+                      <div class="head-bottom">
+                        <div class="bottom"></div>
+                      </div>
+                        </div>
+                     <div class="droid-body">
+                      <div class="circle circle-one">
+                        <div class="inner"></div>
+                      </div>
+                      <div class="circle circle-two"></div>
+                      <div class="circle circle-third">
+                        <div class="inner"></div>
+                      </div>
+                      <div class="dot dot-one"></div>
+                      <div class="dot dot-two"></div>
+                      <div class="stroke stroke-one"></div>
+                      <div class="stroke stroke-two"></div>
+                      <div class="stroke stroke-three"></div>
+                     </div>
+                        <div class="body-shadow-crop">
+                        <div class="bodyshadow"></div></div>
+                  </div>
+               </div>
+               <div class= "starcontainer">
+                    <div class = "deathstar">
+                        <div class = "orbital"></div>
+                        <div class = "innerorb"></div>
+                        <div class = "starlines"></div>
+                    </div>      
+               </div> 
+               <div class = "pixel-block">
+                <div class = "pixelized--vater"></div>
+            </div>     
+            <div class = "r2droidcontainer">
+                <div class = "r2head"></div>
+                <div class = "r2circle1"></div>
+                <div class = "r2circle2"></div>
+                <div class = "r2body"></div>
+                <div class = "r2line1"></div>
+                <div class = "r2line2"></div>
+                <div class = "r2rightleg"></div>
+                <div class = "r2leftleg"></div>
+                <div class = "r2rightfoot"></div>
+                <div class = "r2leftfoot"></div>
+            </div>  
+          </div>
+
+          <footer>
+              <div id="space">
+                <div class="stars"></div>
+                <div class="stars"></div>
+                <div class="stars"></div>
+                <div class="stars"></div>
+                <div class="stars"></div>
+                <div class="logo-facebook-small">
+                  <div class="longpiece"></div>
+                  <div class="longpiece-none"></div>
+                  <div class="shortpiece"></div>
+                  <div class="shortpiece-none"></div>
+                </div>
+                <div class="logo-dropbox-small">
+                  <div class="base-left"></div>
+                  <div class="base-right"></div>
+                  <div class="base-middle"></div>
+                  <div class="cover-left-none"></div>
+                  <div class="cover-right-none"></div>
+                  <div class="cover-top"></div>
+                  <div class="cover-right"></div>
+                  <div class="cover-bottom"></div>
+                  <div class="cover-left"></div>
+                  <div class="box-none"></div>
+                </div>
+                <div class="logo-twitter-small">
+                  <div class="body"></div>
+                  <div class="body-none"></div>
+                  <div class="beak-bottom"></div>
+                  <div class="beak-bottom-none"></div>
+                  <div class="wing-bottom"></div>
+                  <div class="wing-bottom-none"></div>
+                  <div class="wing-middle"></div>
+                  <div class="wing-middle-none"></div>
+                  <div class="wing-top"></div>
+                  <div class="wing-top-none"></div>
+                  <div class="beak-top"></div>
+                  <div class="beak-top-none"></div>
+                  <div class="head"></div>
+                </div>  
+                  <div class="logo-github-small">
+                    <a href=""></a>
+                    <div class="ear-one-left"></div>
+                    <div class="ear-one-none"></div>
+                    <div class="ear-one-right"></div>
+                    <div class="ear-two-right"></div>
+                    <div class="ear-two-none"></div>
+                    <div class="ear-two-left"></div>
+                    <div class="body-bottom"></div>
+                    <div class="body-left-none"></div>
+                    <div class="body-right-none"></div>
+                    <div class="tail-end"></div>
+                    <div class="tail-body"></div>
+                    <div class="tail-body-spots"></div>
+                    <div class="tail-end-none"></div>
+                    <div class="tail-body-none"></div>
+                    <div class="tail-tip"></div>
+                    <div class="body-left"></div>
+                    <div class="body-right"></div>
+                    <div class="head"></div>
+                    <div class="chin-left"></div>
+                    <div class="chin-right"></div>
+                  </div>
+              </div>
+             
+            </footer>
+          
+             
+        </body>
+</html>
